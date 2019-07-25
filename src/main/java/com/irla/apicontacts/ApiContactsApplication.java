8dccffc759b5e48a1fbc3cbbad2b1297a53a39fe
irla <irlasilva808@gmail.com>
2019-07-25 18:33:16 -0300

    Criar projeto com Spring Initializr

---
+++ src/main/java/com/irla/apicontacts/ApiContactsApplication.java
@@ -0,0 +1,13 @@
+package com.irla.apicontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContactsApplication.class, args);
+	}
+
+}
